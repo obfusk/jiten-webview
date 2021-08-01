ea3c6220d733844445caff04090d75cc76c3a4b2
Felix C. Stegerman <flx@obfusk.net>
2021-08-02 01:06:58 +0200

    add server setting

--- app/src/main/java/dev/obfusk/jiten_webview/MainActivity.kt
+++ app/src/main/java/dev/obfusk/jiten_webview/MainActivity.kt
@@ -1,22 +1,56 @@
 package dev.obfusk.jiten_webview
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.net.Uri
 import android.os.Bundle
 import android.webkit.CookieManager
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
-    val server = "jiten.obfusk.dev"
+    val defaultServer = "jiten.obfusk.dev"
+    private var prefs: SharedPreferences? = null
+    private var server: String? = null
     private var webview: WebView? = null
 
-    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        prefs = getPreferences(Context.MODE_PRIVATE)
+        server = prefs?.getString("server", null)
+        if (server == null) chooseServer() else setupWebView()
+    }
+
+    private fun chooseServer(): Unit = AlertDialog.Builder(this).run {
+        val input = EditText(this@MainActivity).apply { setText("https://$defaultServer") }
+        setTitle("Choose a Jiten server")
+        setMessage("(must support https)")
+        setView(input)
+        setPositiveButton(android.R.string.ok) { _, _ ->
+            Uri.parse(input.text.toString().let {
+                if (it.toLowerCase(Locale.getDefault()).startsWith("https://")) it else "https://$it"
+            }).let {
+                if (!it.authority.isNullOrEmpty()) {
+                    server = it.authority
+                    prefs?.edit()?.run { putString("server", server); apply() }
+                    setupWebView()
+                } else {
+                    chooseServer()
+                }
+            }
+        }
+        show()
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun setupWebView() {
         webview = findViewById<WebView>(R.id.webview).apply {
             settings.run {
                 javaScriptEnabled = true
@@ -27,7 +61,7 @@ class MainActivity : AppCompatActivity() {
             webViewClient = object : WebViewClient() {
                 override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean =
                     Uri.parse(url).let {
-                        if (it.host != server) {
+                        if (it.authority != server) {
                             startActivity(Intent(Intent.ACTION_VIEW, it))
                             true
                         } else {
@@ -47,7 +81,13 @@ class MainActivity : AppCompatActivity() {
     override fun onNewIntent(intent: Intent?) {
         super.onNewIntent(intent)
         intent?.data?.toString()?.let { url ->
-            Uri.parse(url).let { if (it.host == server) webview?.loadUrl(url) }
+            Uri.parse(url).let {
+                if (it.authority == defaultServer) {
+                    webview?.loadUrl(it.buildUpon().run {
+                        scheme("https"); authority(server); build().toString()
+                    })
+                }
+            }
         }
     }
 
